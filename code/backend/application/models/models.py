feb80b4a0a00cff7dc9c38e0e39a76c43fd72e1e
Akriti <akriti14220@gmail.com>
2024-03-06 12:58:20 +0530

    commit 3 day1

--- code/backend/application/models/models.py
+++ code/backend/application/models/models.py
@@ -1,8 +1,9 @@
 #this is for db models
 from application.database import db
 import datetime
-# UserMixin for common user attributes and methods
 
+
+#The following table has relationship (many- many) between tickets and the staff assigned
 assigned_staff_tickets = db.Table('assigned_staff_tickets',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('ticket_id', db.Integer, db.ForeignKey('ticket.id'))
@@ -17,12 +18,12 @@ class User(db.Model):
     role_id=db.Column(db.Integer, db.ForeignKey('role.id'))
     role = db.relationship('Role', backref='users')
     card = db.Column(db.String(100), nullable=False)
-    authentication = db.relationship('Authentication', backref='user', uselist=False)
-    disciplinary_actions = db.relationship('DisciplinaryAction', backref='user', uselist=True)
+    authentication = db.relationship('Authentication', backref='user', uselist=False) #to check for tokens
+    disciplinary_actions = db.relationship('DisciplinaryAction', backref='user', uselist=True) # relationship with disciplinary actions.
     assigned_tickets = db.relationship('Ticket', secondary=assigned_staff_tickets,
-                                       backref=db.backref('assigned_staff', lazy='dynamic'))
+                                       backref=db.backref('assigned_staff', lazy='dynamic')) #This is for role = staff for their assigned tickets.
 
-# RoleMixin for common role attributes
+
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
@@ -50,12 +51,12 @@ class Ticket(db.Model):
     comments = db.Column(db.String(500))
     ticket_status=db.Column(db.String(100),nullable=False)
     ticket_priority = db.Column(db.Float)
-    tags = db.relationship('Ticket_Tags',secondary='tickets_tags',backref='tickets')
-    votes = db.relationship('VoteTable', backref='ticket',uselist=True)
+    tags = db.relationship('Ticket_Tags',secondary='tickets_tags',backref='tickets') #tags related to the ticket - many - many relationship
+    votes = db.relationship('VoteTable', backref='ticket',uselist=True) #The votes and who votes one - many relationship
     assigned_staff = db.relationship('User', secondary=assigned_staff_tickets,
-                                     backref=db.backref('assigned_tickets', lazy='dynamic'))
-    comments=db.relationship('TicketComments', backref='ticket',uselist=True)
-    attachments=db.relationship('TicketAttachment', backref='ticket',uselist=True)
+                                     backref=db.backref('assigned_tickets', lazy='dynamic')) #many to many relationship assigned staff
+    comments=db.relationship('TicketComments', backref='ticket',uselist=True) #the comments made by staff to the ticket one - many relationship
+    attachments=db.relationship('TicketAttachment', backref='ticket',uselist=True) #ticket attachments one - many relationship (we can limit to 2)
 
 class VoteTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -64,10 +65,11 @@ class VoteTable(db.Model):
 
 class TicketTags(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
-    tags = db.Column(db.String(100))
-    name = db.Column(db.String(100))
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id')) #connected to ticket 
+    tag_name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
 
+#Following is the relationship table between tickets and tags 
 tickets_tags=db.Table('tickets_tags',
     db.Column('ticket_id',db.Integer,db.ForeignKey('ticket.id')),
     db.Column('tag_id',db.Integer,db.ForeignKey('tag.id'))
