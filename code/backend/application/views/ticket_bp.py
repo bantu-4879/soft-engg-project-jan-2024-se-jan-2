0f65e6603de66f07b786147e421cbaabb22f1ea3
Sahithi D <38768509+sahithid@users.noreply.github.com>
2024-03-12 14:11:03 +0530

    Added the bp files

---
+++ code/backend/application/views/ticket_bp.py
@@ -0,0 +1,16 @@
+# - get specific ticket *
+# - post a new ticket *  # add the details to the ticket_tags 
+# - update a ticket *
+# - delete a ticket *
+# - vote a ticket  *
+
+
+
+# - solving a ticket & 
+# - assigning a ticket & 
+# - get tickets by assigned tickets &
+
+# - get comments of a ticket % 
+# - post comment &
+# - edit comment &
+# - delete comment &
---
+++ code/backend/application/views/ticket_tracking_bp.py
@@ -0,0 +1,6 @@
+        # a - change ticket status 
+        # b- change priority (manual) 
+        # c- create ticket data - ( open , time resolve etc )
+        # d- send notifications when assigning -- > jobs 
+        # e- send status report 
+        # f- escalation + ( adding a new entry to the ticket data table , with the same ticket id )
