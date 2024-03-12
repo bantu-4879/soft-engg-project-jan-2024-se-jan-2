049ac6e7b52f5e0cbf9e40f4579fe7849f03c9f6
Akriti <akriti14220@gmail.com>
2024-03-12 14:19:41 +0530

    commit_new

---
+++ code/backend/application/views/ticket_bp.py
@@ -0,0 +1,31 @@
+# --------------------  Imports  --------------------
+
+from flask import Blueprint, request
+from flask_restful import Api, Resource
+from application.logger import logger
+from application.common_utils import (
+    token_required,
+    users_required,
+)
+from application.views.user_utils import UserUtils
+from application.responses import *
+from application.models import User, Ticket,VoteTable
+from application.globals import *
+
+# --------------------  Code  --------------------
+
+#Sahithi 
+
+
+#Akriti 
+
+
+# Muskan 
+
+
+
+
+
+
+
+
