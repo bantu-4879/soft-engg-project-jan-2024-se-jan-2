32bb5132031c371c572b6fcb88b6f87a12fd4198
Akriti <akriti14220@gmail.com>
2024-04-03 17:27:34 +0530

    commit for discourse APIs
    This has added new discourse APIs

--- code/backend/application/views/discoursePost_bp.py
+++ code/backend/application/views/discoursePost_bp.py
@@ -27,7 +27,7 @@ discoursePost_bp=Blueprint("discoursePost_bp",__name__)
 discoursePost_api=Api(discoursePost_bp)
 discourseUserUtils=DiscourseUserUtils()
 
-class Categories(Resource):
+class AllCategories(Resource):
     """
     usage 
     ------
@@ -39,14 +39,13 @@ class Categories(Resource):
 
     
     """
-
-    @token_required
-    @users_required(users=['Student','Staff','Admin'])
+    #@token_required
+    #@users_required(users=['Student','Staff','Admin'])
     def get(self):
         headers={
             'Api-Key':API_KEY,
             'Api-Username':API_USERNAME,
-            "include_subcategories":True
+            "include_subcategories":'true'
         }
         url=f'{DISCOURSE_BASE_URL}/categories.json'
         response=requests.get(url,headers=headers)
@@ -67,12 +66,170 @@ class Categories(Resource):
         else:
             raise NotFoundError(status_msg="could not load categories")
 
+class Categories(Resource):
+    """
+    usage 
+    ------
+    This gives the details about a single categpry and then let the students create post in that category.
+    
+    parameters 
+    -------
+    this takes category id and returns details.
+
+    
+    """
+
+    #@token_required
+    def get(self,category_id):
+        headers={
+            'Api-Key':API_KEY,
+            'Api-Username':API_USERNAME,
+            "include_subcategories":'true'
+        }
+        url=f'{DISCOURSE_BASE_URL}/c/{category_id}/show.json'
+        response=requests.get(url,headers=headers)
+        if(response.status_code == 200):
+            response=response.json()
+            category=response["category"]
+            data={}
+            data["id"] = category["id"]
+            data["name"] = category["name"]
+            data["color"] = category["color"]
+            data["text_color"] = category["text_color"]
+            data["description"] = category["description"]
+            return success_200_custom(data=data)
+        else:
+            raise NotFoundError(status_msg="could not load category data")
+
+    #@token_required
+    #@users_required(users=['Staff','Admin'])
+    def post(self):
+        headers={
+            'Api-Key':API_KEY,
+            'Api-Username':API_USERNAME
+        }
+        form={}
+
+        try:
+            form =request.get_json()
+            category_name=form.get("name","")
+            color=form.get("color", "") 
+            text_color=form.get("text-color","")
+            permission_staff=form.get("Staff",0)
+            parent_category_id=form.get("parent_category","")
+            permission_all=form.get("All",1)
+            category_description=form.get("description","")
+        except Exception as e:
+            logger.error(f"DiscourseCategoryCreation ->Error occured while getting form data : {e} ")
+            raise InternalServerError
+        else:
+            if discourseUserUtils.is_blank(category_name) or discourseUserUtils.is_blank(color) or discourseUserUtils.is_blank(text_color):
+                raise BadRequest(status_msg=f"Category Name, Color or text color cannot be empty")
+            if self.category_exists(category_name=category_name):
+                raise AlreadyExistError(status_msg="The category with given name already exists.")
+            payload={
+                "name":category_name,
+                "color":color,
+                "text_color":text_color,
+                "parent_category_id":parent_category_id,
+                "permissions":{
+                    "everyone":permission_all,
+                    "staff":permission_staff
+                },
+                "description":category_description
+            }
+            headers={
+                'Api-Key':API_KEY,
+                'Api-Username':API_USERNAME
+            }
+            url=f'{DISCOURSE_BASE_URL}/categories.json'
+            response=requests.post(url,json=payload,headers=headers)
+            if response.status_code == 200:
+                logger.info("Success.")
+                raise Success_200(status_msg="category created successfully",status_code=response.status_code)
+            else:
+                raise BadRequest(
+                    status_msg=response.error
+                )
         
+    def category_exists(self,category_name):
+        headers={
+            'Api-Key':API_KEY,
+            'Api-Username':API_USERNAME,
+            "include_subcategories":'false'
+        }
+        url=f'{DISCOURSE_BASE_URL}/categories.json'
+        response=requests.get(url,headers=headers)
+        if(response.status_code == 200):
+            response=response.json()
+            print(response.keys())
+            categories=response["category_list"]["categories"]
+            for category in categories:
+                if(str.lower(category["name"])==str.lower(category_name)):
+                    return True
+            return False
     
 
 class Tags(Resource):
-    print("resource")
-    #get,#post
+    def __init__(self,user_id=None):
+        self.user_id=user_id
+
+    
+    @token_required
+    def get(self):
+        headers={
+            'Api-Key':API_KEY,
+            'Api-Username':API_USERNAME,
+        }
+        url=f'{DISCOURSE_BASE_URL}/tag_groups.json'
+        response=requests.get(url,headers=headers)
+        if(response.status_code == 200):
+            response=response.json()
+            tag_groups=response["tag_groups"]
+            data=[]
+            for tag in tag_groups:
+                _d = {}
+                _d["id"] = tag["id"]
+                _d["name"] = tag["name"]
+                _d["tag_names_list"] = tag["tag_names"]
+                _d["permission"] = tag["permission"]
+                data.append(_d)
+            return success_200_custom(data=data)
+        else:
+            raise NotFoundError(status_msg="could not load Tags data")
+    
+    @token_required
+    @users_required(users='Staff')
+    def post(self):
+        try:
+            form = request.get_json()
+            tag_name = form.get("name", "")
+        except Exception as e:
+            logger.error(f"DiscourseTagCreation: Post ->Error occured while getting form data : {e} ")
+            raise InternalServerError
+
+        # Validate form data
+        if discourseUserUtils.is_blank(tag_name):
+            raise BadRequest(status_msg=f"Tag Name cannot be empty")
+
+        # Create payload
+        payload = {
+            "name": tag_name
+        }
+        # Send request to Discourse API
+        headers = {'Api-Key': API_KEY, 'Api-Username': API_USERNAME}
+        url = f'{DISCOURSE_BASE_URL}/tags.json'
+        try:
+            response = requests.post(url, json=payload, headers=headers)
+            response_data = response.json()
+            if response.status_code == 200:
+                return response_data, 200
+            else:
+                return {"error": response_data.get("error", "Unknown error")}, response.status_code
+        except Exception as e:
+            return {"error": f"Error occurred while making request to Discourse API: {e}"}, 500
+
+
 
 
 class DiscourseTopics(Resource):
@@ -91,7 +248,7 @@ class TicketThread(Resource):
 
 
 
-
-discoursePost_api.add_resource(Categories,"/categories")
-        
+discoursePost_api.add_resource(AllCategories,"/categories")
+discoursePost_api.add_resource(Categories,"/category","/category/<int:category_id>")
+discoursePost_api.add_resource(Tags,'/tags')
 
