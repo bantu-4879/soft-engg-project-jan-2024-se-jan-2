afb3d48beae78e62906cddeda9b7366a7663c1bb
Aman kankriya <bantukankriya@gmail.com>
2024-04-11 08:53:09 +0530

    updated common.js

--- code/frontend/src/assets/common.js
+++ code/frontend/src/assets/common.js
@@ -9,6 +9,8 @@ const SUPPORT_API_PREFIX = `/api/${VERSION}/staff`;
 const ADMIN_API_PREFIX = `/api/${VERSION}/admin`;
 const FAQ_API_PREFIX = `/api/${VERSION}/faq/`;
 const TICKET_API_PREFIX = `/api/${VERSION}/ticket`
+const DISCOURSE_AUTH_PREFIX = `api/${VERSION}/discourseAuth`
+const DISCOURSE_POST_PREFIX = `api/${VERSION}/discourse`
 
 const AUTH_API_LOGIN = `${BASEURL}${AUTH_API_PREFIX}/login`
 const AUTH_API_REGISTER = `${BASEURL}${AUTH_API_PREFIX}/register`
@@ -19,15 +21,19 @@ const SUPPORT_API = `${BASEURL}${SUPPORT_API_PREFIX}`;
 const ADMIN_API = `${BASEURL}${ADMIN_API_PREFIX}`;
 const FAQ_API = `${BASEURL}${FAQ_API_PREFIX}`;
 const TICKET_API = `${BASEURL}${TICKET_API_PREFIX}`;
+const DISCOURSE_REGISTER_API = `${BASEURL}${DISCOURSE_AUTH_PREFIX}`
+
 
 const TICKET_API_ALLTICKETS = `${BASEURL}${TICKET_API_PREFIX}/all-tickets`
 const WEBHOOK_URL_STAFF = `https://chat.googleapis.com/v1/spaces/AAAAsTbnkos/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=2ZVePrYB_1cEwSrlkdUstzpUGCFBW9a6_RtzylmGdQE`
 const WEBHOOK_URL_STUDENT = `https://chat.googleapis.com/v1/spaces/AAAAsTbnkos/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=aJjfYWhYMscOv6l-VukfKhX3c3GhRmN3Xpg_KAt03p4`
 const WEBHOOK_URL_ADMIN = `https://chat.googleapis.com/v1/spaces/AAAAsTbnkos/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=VgCumRm7zG5aykpSjHoQ9bZ9FG3i2LSZEdmMlcD_CBA`
 
+
 const STUDENT_ROUTES = ['/student', '/student-home', "/student-create-ticket", "/student-my-tickets", "/common-faqs", "/user-profile"]
 const SUPPORT_ROUTES = ['/staff', '/staff-home', "/staff-my-tickets", "/common-faqs", "/user-profile"]
 const ADMIN_ROUTES = ['/admin', '/admin-home', "/admin-create-faq", "/admin-validate-users", "/common-faqs", "/user-profile","/inbox"]
+
 
 const STUDENT_NAV_BUTTONS = [
   { id: 1, title: "Home", link: "/student-home", active: false },
@@ -77,5 +83,6 @@ export {
   ADMIN_NAV_BUTTONS, 
   WEBHOOK_URL_ADMIN,
   WEBHOOK_URL_STUDENT,
-  WEBHOOK_URL_STAFF
+  WEBHOOK_URL_STAFF,
+  DISCOURSE_REGISTER_API
 };
